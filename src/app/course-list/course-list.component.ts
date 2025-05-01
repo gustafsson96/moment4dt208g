478c2ce5909e8325c3b9f3cdeaeae6076f7741b0
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-01 11:18:24 +0200

    Use *ngFor to display courses to table

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoursesService } from '../services/courses.service';
+import { Course } from '../models/course';
+
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css'
 })
 export class CourseListComponent {
+  courses: Course[] = [];
+
+  constructor(private courseService : CoursesService) {}
+
+  ngOnInit() {
+    this.courseService.getCourses().subscribe((data) => {
+      this.courses = data;
+    })
+  }
 
 }
