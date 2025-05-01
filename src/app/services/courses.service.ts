a872debe2ee3613dcb7ae242ce76626c1c91682f
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-01 11:00:56 +0200

    Access json file via HttpClient

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  private url: string = '/ramschema.json';
+
+  constructor(private http: HttpClient) { }
+
+  // Get courses
+  getCourses() : Observable<any[]> {
+    return this.http.get<any>(this.url);
+  }
 }
