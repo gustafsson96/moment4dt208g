e648940381a3e3f5e7bf38763245b9b4ae761896
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-05-01 17:50:40 +0200

    Add input field with course filtering functionality

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -2,24 +2,35 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesService } from '../services/courses.service';
 import { Course } from '../models/course';
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css'
 })
 export class CourseListComponent {
   courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  filteredValue: string = "";
 
   constructor(private courseService : CoursesService) {}
 
   ngOnInit() {
     this.courseService.getCourses().subscribe((data) => {
       this.courses = data;
+      this.filteredCourses = data;
     })
+  }
+
+  applyFilter(): void {
+    this.filteredCourses = this.courses.filter((data) =>
+      data.coursename.toLowerCase().includes(this.filteredValue.toLowerCase())
+    )
   }
 
 }
